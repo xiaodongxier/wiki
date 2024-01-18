faf503d7eb31a28f827f50dbc8842484bc47056a
xiaodongxier <xiaodongxier@gmail.com>
2024-01-18 04:04:02 +0000

    自动化部署完成

---
+++ assets/javascripts/custom.js
@@ -0,0 +1,9 @@
+setTimeout(() => {
+  const element = document.getElementsByClassName("md-copyright")[0];
+const busuanzi = `
+<span id="busuanzi_container_site_pv">
+    本站总访问量<span id="busuanzi_value_site_pv"></span>次
+</span>
+`;
+element.insertAdjacentHTML('afterend', busuanzi);
+}, 1000);

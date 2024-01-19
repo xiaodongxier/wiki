88b74409383aa5de92862bcf926805fc37aa750b
xiaodongxier <xiaodongxier@gmail.com>
2024-01-19 09:17:49 +0000

    自动化部署完成

---
+++ assets/javascripts/baidu.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?c21a66ab5099adc6454e09ad1f8301e7";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
--- assets/javascripts/custom.js
+++ assets/javascripts/custom.js
@@ -1,9 +1,16 @@
-setTimeout(() => {
-  const element = document.getElementsByClassName("md-copyright")[0];
-  const busuanzi = `
-  <span id="busuanzi_container_site_pv">
-      本站总访问量<span id="busuanzi_value_site_pv"></span>次
-  </span>
-  `;
-  element.insertAdjacentHTML('afterend', busuanzi);
-}, 1000);
+// setTimeout(() => {
+//   const element = document.getElementsByClassName("md-copyright")[0];
+//   const busuanzi = `
+//   <span id="busuanzi_container_site_pv">
+//       本站总访问量<span id="busuanzi_value_site_pv"></span>次
+//   </span>
+//   `;
+//   element.insertAdjacentHTML('afterend', busuanzi);
+// }, 1000);
+
+
+// window.onload = function () {
+//   const element = document.querySelector(".md-copyright");
+//   const busuanzi = document.querySelector(".heng + span").innerHTML;
+//   element.insertAdjacentHTML('afterend', `<div>${busuanzi}</div>`);
+// }

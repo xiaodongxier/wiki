46fc3d4663fb88b894b346f6db241db7292368f4
xiaodongxier <xiaodongxier@gmail.com>
2024-01-23 02:32:14 +0000

    自动化部署完成

--- assets/javascripts/51LA.js
+++ assets/javascripts/51LA.js
@@ -1,4 +1,5 @@
-// !function(p){"use strict";!function(t){var s=window,e=document,i=p,c="".concat("https:"===e.location.protocol?"https://":"http://","sdk.51.la/js-sdk-pro.min.js"),n=e.createElement("script"),r=e.getElementsByTagName("script")[0];n.type="text/javascript",n.setAttribute("charset","UTF-8"),n.async=!0,n.src=c,n.id="LA_COLLECT",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:"Jf8rVs7oam5pFYhA",ck:"Jf8rVs7oam5pFYhA"});
+!function(p){"use strict";!function(t){var s=window,e=document,i=p,c="".concat("https:"===e.location.protocol?"https://":"http://","sdk.51.la/js-sdk-pro.min.js"),n=e.createElement("script"),r=e.getElementsByTagName("script")[0];n.type="text/javascript",n.setAttribute("charset","UTF-8"),n.async=!0,n.src=c,n.id="LA_COLLECT",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:"Jf8rVs7oam5pFYhA",ck:"Jf8rVs7oam5pFYhA"});
+
 (function() {
   var script = document.createElement("script");
   script.id = "LA-DATA-WIDGET";
